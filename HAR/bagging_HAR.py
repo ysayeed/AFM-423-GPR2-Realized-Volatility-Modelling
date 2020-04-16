0acd6b353f8fb5394a2b753975f74cd1b1d9ee66
carolinepie <peizhenning@gmail.com>
2020-04-16 16:54:50 -0400

    adding bagging; also fixing line endings

--- HAR/bagging_HAR.py
+++ HAR/bagging_HAR.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import BaggingRegressor
 import matplotlib.pyplot as plt
 
 dataset = 'SPX'
@@ -12,7 +13,6 @@ print(pred)
 daily = pred.shift(1)
 weekly = daily.rolling(5).mean()
 monthly = daily.rolling(22).mean()
-
 
 daily.rename(columns={'Realized Volatility': 'Realized Volatility Prev Day'}, inplace=True)
 weekly.rename(columns={'Realized Volatility': 'Realized Volatility Weekly'}, inplace=True)
@@ -36,27 +36,38 @@ x_labels = [
 
 y_label = 'Realized Volatility'
 
-# print(train)
-# print(test)
-# HAR model fit:
-har = LinearRegression().fit(train[x_labels], train[y_label])
-# print(har.coef_)
-# print(har.intercept_)
+# HAR model fit: pick best number of estimators:
+har_bag = None
+best_n = 0
+lowest_test_mse = 100
+for i in range(1, 21):
+    model = BaggingRegressor(base_estimator=LinearRegression(),
+                               n_estimators=i, random_state=0) \
+             .fit(train[x_labels], train[y_label])
 
+    mse = mean_squared_error(test[y_label], model.predict(test[x_labels]))
+    # print(mse)
+    if mse < lowest_test_mse:
+        lowest_test_mse = mse
+        har_bag = model
+        best_n = i
+
+
+print(best_n)
 # out sample:
-test_pred = har.predict(test[x_labels])
+test_pred = har_bag.predict(test[x_labels])
 mse = mean_squared_error(test[y_label], test_pred)
 print(mse)
 
 plt.plot(test_pred, color='orange')
 plt.plot(test[y_label], color='green')
-plt.savefig('har_test.png')
+plt.savefig('bag_har_test.png')
 
 # in and out sample trend:
-train_test_pred = har.predict(df[x_labels])
+train_test_pred = har_bag.predict(df[x_labels])
 mse = mean_squared_error(df[y_label], train_test_pred)
 print(mse)
 
 plt.plot(train_test_pred, color='orange')
 plt.plot(df[y_label], color='green')
-plt.savefig('har_train_test.png')
+plt.savefig('bag_har_train_test.png')
